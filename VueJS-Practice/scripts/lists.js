15e54b7be72d65dd4466755c9d9a8ee6f90d68f7
Vlad Mocanu <vladmoca90@gmail.com>
2018-02-03 21:38:15 +0000

    Saved the entire project

--- VueJS-Practice/scripts/lists.js
+++ VueJS-Practice/scripts/lists.js
@@ -1,9 +1,19 @@
-﻿new Vue({
-    el: "#intro",
-    data: {
-        names: ["Joe", "Mary", "Jane", "Jack", "Erika"],
-        styleList: {
-            fontSize: '15px'
-        }
-    }
-});
+﻿<script type="text/javascript">
+
+        new Vue({
+
+            el: "#list-container",
+            data: {
+                names: ["Joe", "Mary", "Jane", "Jack", "Erika"],
+                styleList: {
+                    fontSize: '15px',
+                    marginTop: '25px',
+                    maxWidth: '125px',
+                    textAlign: 'center'
+                },
+                listGroup: 'list-group center-block',
+                listGroupItem: 'list-group-item'
+            }
+        });
+
+    </script>
