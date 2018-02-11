88df0863a4a367c24f7452464f2be0c041a2b34e
Vlad Mocanu <vladmoca90@gmail.com>
2018-02-11 21:23:59 +0000

    Declared the prop

---
+++ VueJS-Practice/scripts/first-prop.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div class="u-full-width"></div>
+</template>
+
+<script>
+export default {
+
+    props: [
+        type: Object,
+    ],
+    data() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
