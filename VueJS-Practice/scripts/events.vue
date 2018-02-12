45dc3e075be4825fabe92a0824b01ee2e2ac1b49
Vlad Mocanu <vladmoca90@gmail.com>
2018-02-12 21:58:49 +0000

    Merge branch 'master' into develop
    Created the code structure for the @click.once

--- VueJS-Practice/scripts/events.vue
+++ VueJS-Practice/scripts/events.vue
@@ -1,1 +1,20 @@
+<template lang="html">
+  <div class="u-full-width">
+      <div class="btn btn-success" @click.once="oneClick">Press this button</div>
+  </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+
+        props: ["count"],
+        data() {
+
+            oneClick {
+                
+            }
+        }
+    }
+</script>
+
 <style lang="scss" scoped></style>
