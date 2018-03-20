79a9ef8cc20e5fd7c029644ca9742ed5910be0c1
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-20 20:59:36 +0000

    The gulpfile.js is ready

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    return gulp.src('content/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('content/'))
+});
+
+//Watch task
+gulp.task('default',function() {
+    return gulp.watch('content/**/*.scss',['sass']);
+});
