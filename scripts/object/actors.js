87d70a3f1b5df7db330e21d449d32657c9cf1937
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-07 23:00:47 +0100

    Added two more files in the object project

---
+++ scripts/object/actors.js
@@ -0,0 +1,35 @@
+new Vue({
+    el: '.box',
+    data: {
+        query: '',
+        actors: [{
+                name: 'Russell Crowe',
+                dateOfBirth: '7 April 1964',
+                placeOfBirth: 'Wellington, New Zealand',
+                biography: 'Russell Ira Crowe is an actor, film producer and musician. Although a New Zealand citizen, he has lived most of his life in Australia. He came to international attention for his role as the Roman General Maximus Decimus Meridius in the 2000 historical epic film Gladiator, directed by Ridley Scott, for which Crowe won an Academy Award for Best Actor, a Broadcast Film Critics Association Award for Best Actor, an Empire Award for Best Actor and a London Film Critics Circle Award for Best Actor and 10 further nominations for best actor.',
+            },
+            {
+                name: 'Natalie Portman',
+                dateOfBirth: '9 June 1981',
+                placeOfBirth: 'Jerusalem, Israel',
+                biography: 'Natalie Portman (born Neta-Lee Hershlag) is an actress and film producer with dual Israeli and American citizenship. Portman is best known for her roles as Padmé Amidala in the Star Wars prequel trilogy and Nina Sayers in Black Swan (2010); she won an Academy Award, Golden Globe Award, and Screen Actors Guild Award, among other accolades, for her performance in the latter.',
+            },
+            {
+                name: 'Gerard Butler',
+                dateOfBirth: '13 November 1969',
+                placeOfBirth: 'Paisley, Scotland',
+                biography: 'Gerard James Butler is a Scottish actor, producer, and singer. After studying law, Butler turned to acting in the mid-1990s with small roles in productions such as Mrs Brown (1997), the James Bond film Tomorrow Never Dies (1997), and Tale of the Mummy (1998). In 2000, he starred as Dracula in the horror film Dracula 2000 with Christopher Plummer and Jonny Lee Miller.',
+            },
+        ],
+    },
+    computed: {
+        allActors: function () {
+            return this.actors.filter((actor) => {
+                return actor.name.match(this.query) ||
+                    actor.dateOfBirth.match(this.query) ||
+                    actor.placeOfBirth.match(this.query) ||
+                    actor.biography.match(this.query)
+            });
+        }
+    }
+});
---
+++ scripts/object/counties.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '.filter-container',
+    data: {
+        query: '',
+        counties: [{
+            title: 'Berskshire',
+            content: 'Berkshire is a county in south east England, west of London and is one of the home counties. It was recognised by the Queen as the Royal County of Berkshire in 1957 because of the presence of Windsor Castle, and letters patent were issued in 1974. Berkshire is a county of historic origin and is a home county',
+        }, {
+            title: 'Surrey',
+            content: 'Get the latest news from the BBC in Berkshire: Local website with breaking news, sport, weather and travel from the area plus in-depth features, analysis, audio and video coverage; listen to BBC Radio Berkshire live and catch-up with local TV news.',
+        }, {
+            title: 'Lincolnshire',
+            content: 'With so many places to visit in Berkshire, you can easily forget you are right on London’s doorstep. ... Berkshire is also home to many other picturesque towns and villages such as Newbury, Hungerford, Ascot, Hurley, Cookham, Bray, Littlewick Green, and Waltham St Lawrence',
+        }],
+    },
+    computed: {
+        allCounties: function () {
+            return this.counties.filter((county) => {
+                return county.content.match(this.query) ||
+                    county.title.match(this.query)
+            });
+        }
+    },
+});
