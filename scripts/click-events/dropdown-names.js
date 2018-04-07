684d3d83dc1e750370dc16aae6eb9a05908cbc21
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-07 20:37:05 +0100

    CReated a separate .js file for the dropdown

---
+++ scripts/click-events/dropdown-names.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '.dropdown-box',
+    data: {
+        showNames: false,
+        arrowDown: false,
+        persons: [
+            "Danielle McDonald",
+            "John Swift",
+            "Angela Wycombe",
+            "Samuel L Jackson",
+            "Juliana Pike",
+        ],
+    },
+});
