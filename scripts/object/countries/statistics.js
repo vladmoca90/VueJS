7cda189c14df94f8ae2f1ad3b4fe6994021a6e36
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-10 03:45:31 +0100

    Renamed the names of the .js files from the countries project

---
+++ scripts/object/countries/statistics.js
@@ -0,0 +1,94 @@
+new Vue({
+    el: '.countries__statistics',
+    data: {
+        countries: [
+            {
+                name: "Portugal",
+                population: 10, //in millions
+                capital: "Lisbon",
+                language: "Portuguese",
+                otherCities: ["Porto", "Funchal", "Faro"],
+                UNmember: true,
+            },
+            {
+                name: "United States",
+                population: 315, //in millions
+                capital: "Washington DC",
+                language: "English",
+                otherCities: ["Chicago", "Los Angeles", "New York"],
+                UNmember: true,
+            },
+            {
+                name: "Israel",
+                population: 8, //in millions
+                capital: "Jerusalem",
+                language: "Hebrew",
+                otherCities: ["Tel Aviv", "Haifa", "Eilat"],
+                UNmember: true,
+            },
+            {
+                name: "Australia",
+                population: 24, //in millions
+                capital: "Canberra",
+                language: "English",
+                otherCities: ["Brisbane", "Melbourne", "Sydney"],
+                UNmember: true,
+            },
+            {
+                name: "Brazil",
+                population: 193, //in millions
+                capital: "Brasilia",
+                language: "Portuguese",
+                otherCities: ["Rio de Janeiro", "Recife", "Sao Paulo"],
+                UNmember: true,
+            },
+            {
+                name: "Palestine",
+                population: 4, //in millions
+                capital: "Ramallah",
+                language: "Arabic",
+                otherCities: ["Bethleehem", "Gaza", "Hebron"],
+                UNmember: false,
+            },
+            {
+                name: "India",
+                population: 1135, //in millions
+                capital: "New Delhi",
+                language: "Hindi",
+                otherCities: ["Madras", "Mumbai", "Varanasi"],
+                UNmember: true,
+            }, {
+                name: "Morocco",
+                population: 33, //in millions
+                capital: "Rabat",
+                language: "Arabic",
+                otherCities: ["Fes", "Marrakesh", "Casablanca"],
+                UNmember: true,
+            },
+        ],
+    },
+    methods: {
+        totalPopulation: function (countries) {
+
+            if (countries.length == 0) {
+                throw new Error('The array is empty');
+            }
+
+            if (countries.length == 1) {
+                return countries[0].population;
+            }
+
+            var total = countries[0].population;
+
+            for (i = 0; i < countries.length; i++) {
+                total += countries[i].population;
+            }
+
+            return total;
+        },
+        numberOfDifferentLang: function () {
+            var counter = 0;
+
+        },
+    },
+});
