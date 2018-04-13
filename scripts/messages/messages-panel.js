ea5b2f78cae58857676d3aaff60281cd193252e9
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-13 21:38:21 +0100

    Added new styling and folders to the messages-panel

---
+++ scripts/messages/messages-panel.js
@@ -0,0 +1,16 @@
+ new Vue({
+        el: '.messages-panel',
+        data: {
+            query: '',
+            stayActive: false,
+            writeComment: '',
+            profile: [{
+                img: '',
+                name: '',
+                status: '',
+            }],
+            conversations: [],
+        },
+        computed: {},
+        methods: {},
+    });
