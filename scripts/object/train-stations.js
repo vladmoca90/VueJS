7081ecf5a23141e651a03bc6b7da0e98f3539c8d
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-21 16:11:25 +0100

    Working on a new project with train stations

---
+++ scripts/object/train-stations.js
@@ -0,0 +1,117 @@
+new Vue({
+    el: '',
+    data: {
+        stations: [
+            {
+                code: 'BSK',
+                name: 'Basingstoke',
+            },
+            {
+                code: 'BTH',
+                name: 'Bath Spa',
+            },
+            {
+                code: 'BHM',
+                name: 'Birmingham Grand Central',
+            },
+            {
+                code: 'BAW',
+                name: 'Blackwater',
+            },
+            {
+                code: 'BCE',
+                name: 'Bracknell',
+            },
+            {
+                code: 'BRI',
+                name: 'Bristol Temple Meads',
+            },
+            {
+                code: 'CDF',
+                name: 'Cardiff Central',
+
+            },
+            {
+                code: 'DID',
+                name: 'Didcot Parkway',
+
+            },
+            {
+                code: 'EAL',
+                name: 'Ealing Broadway',
+
+            },
+            {
+                code: 'FNN',
+                name: 'Farnborough North',
+            },
+            {
+                code: 'GTW',
+                name: 'Gatwick Airport',
+            },
+            {
+                code: 'GLD',
+                name: 'Guildford',
+            },
+            {
+                code: 'PAD',
+                name: 'London Paddington',
+            },
+            {
+                code: 'WAT',
+                name: 'London Waterloo',
+            },
+            {
+                code: 'MAI',
+                name: 'Maidenhead',
+            },
+            {
+                code: 'MIA',
+                name: 'Manchester Picadilly',
+            },
+            {
+                code: 'NBY',
+                name: 'Newbury',
+            },
+            {
+                code: 'NCL',
+                name: 'Newcastle',
+            },
+            {
+                code: 'OXF',
+                name: 'Oxford',
+            },
+            {
+                code: 'PLY',
+                name: 'Plymouth',
+            },
+            {
+                code: 'RDG',
+                name: 'Reading',
+            },
+            {
+                code: 'SOU',
+                name: 'Southampton Central',
+            },
+            {
+                code: 'TWI',
+                name: 'Twickenham',
+            },
+            {
+                code: 'WIN',
+                name: 'Winchester',
+            },
+            {
+                code: 'WNC',
+                name: 'Windsor',
+            },
+            {
+                code: 'WKM',
+                name: 'Wokingham',
+            },
+        ],
+    },
+    methods: {
+
+    },
+});
