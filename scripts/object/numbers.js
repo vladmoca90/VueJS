b9c5bc1499f147c483274d1daf2a7e1ceaa94213
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-22 14:00:38 +0100

    Added an image to the actors project

--- scripts/object/numbers.js
+++ scripts/object/numbers.js
@@ -1,14 +1,14 @@
 new Vue({
     el: '.table-box',
     data: {
-        numbers: [1.79320, 4.45867, 10.55563, 7.00256, 3.3365, 4.56354, 7.76500],
+        numbers: [1.79320, 4.45867, 10.55563, 7.00256, 3.33658, 4.56354, 7.76510],
     },
     methods: {
-        toFixedValue: function (number) {
+        fixedValue: function (number) {
             return number.toFixed(2);
         },
-        toPrecisionValue: function (number) {
-            return number.toPrecision(5);
+        precisionValue: function (number) {
+            return number.toPrecision(4);
         },
         powerValue: function (number) {
             return Math.pow(number, 6).toFixed(2);
