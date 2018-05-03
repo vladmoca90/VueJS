92d13435369fee9bb30d1707233d7e24b9e07f65
Vlad Mocanu <vladmoca90@gmail.com>
2018-05-03 20:38:02 +0100

    Test

--- JET-Test/App.vue
+++ JET-Test/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <table>
+    <table class="table table-bordered">
         <thead>
             <th></th>
         </thead>
@@ -12,79 +12,80 @@
 </template>
 
 <script>
-data() {
-    {
-    "d": {
-        "__type": "eventdata.co.uk.APIMasterPersonPreference+PreferenceAPIResponseJSON",
-        "Preferences": [
+    export default {
+        data() {
             {
-                "__type": "eventdata.co.uk.PreferenceJSON",
-                "ControllerCode": "TEST1",
-                "PreferenceCode": "Root",
-                "Name": "My Data Controller",
-                "Description": "Would you like to hear from My Data Controller (*)?",
-                "Status": "Active"
-            },
-            {
-                "__type": "eventdata.co.uk.PreferenceJSON",
-                "ControllerCode": "TEST1",
-                "PreferenceCode": "Root.Conferences",
-                "Name": "Conferences",
-                "Description": "Would you like to hear periodic updates about our conferences (*)?",
-                "Status": "Active"
-            },
-            {
-                "__type": "eventdata.co.uk.PreferenceJSON",
-                "ControllerCode": "TEST1",
-                "PreferenceCode": "Root.Delete",
-                "Name": "don't delete me",
-                "Description": "don't delete me... (*)",
-                "Status": "Hidden"
-            },
-            {
-                "__type": "eventdata.co.uk.PreferenceJSON",
-                "ControllerCode": "TEST1",
-                "PreferenceCode": "Root.Events",
-                "Name": "Events",
-                "Description": "Do you want to read more about Events (*)?",
-                "Status": "Active"
-            },
-            {
-                "__type": "eventdata.co.uk.PreferenceJSON",
-                "ControllerCode": "TEST1",
-                "PreferenceCode": "Root.Exhibitions",
-                "Name": "Exhibitions",
-                "Description": "Would you like to hear periodic updates on our exhibitions (*)?",
-                "Status": "Active"
-            },
-            {
-                "__type": "eventdata.co.uk.PreferenceJSON",
-                "ControllerCode": "TEST1",
-                "PreferenceCode": "Root.Exhibitions.HealthCare",
-                "Name": "Heath Care",
-                "Description": "Would you like to hear periodic updates about our Health Care exhibition  (*)?",
-                "Status": "Active"
-            },
-            {
-                "__type": "eventdata.co.uk.PreferenceJSON",
-                "ControllerCode": "TEST1",
-                "PreferenceCode": "Root.Exhibitions.HealthCare.3rdPartyPartnerCommLtd",
-                "Name": "3rd Party - Partner Comm Ltd.",
-                "Description": "Is it OK to share information with Partner Comm Ltd. to hear about their service  (*)?",
-                "Status": "Active"
-            },
-            {
-                "__type": "eventdata.co.uk.PreferenceJSON",
-                "ControllerCode": "TEST1",
-                "PreferenceCode": "Root.Exhibitions.Training",
-                "Name": "Training",
-                "Description": "Would you like to hear about training?",
-                "Status": "Active"
+                "d": {
+                    "__type": "eventdata.co.uk.APIMasterPersonPreference+PreferenceAPIResponseJSON",
+                    "Preferences": [{
+                            "__type": "eventdata.co.uk.PreferenceJSON",
+                            "ControllerCode": "TEST1",
+                            "PreferenceCode": "Root",
+                            "Name": "My Data Controller",
+                            "Description": "Would you like to hear from My Data Controller (*)?",
+                            "Status": "Active"
+                        },
+                        {
+                            "__type": "eventdata.co.uk.PreferenceJSON",
+                            "ControllerCode": "TEST1",
+                            "PreferenceCode": "Root.Conferences",
+                            "Name": "Conferences",
+                            "Description": "Would you like to hear periodic updates about our conferences (*)?",
+                            "Status": "Active"
+                        },
+                        {
+                            "__type": "eventdata.co.uk.PreferenceJSON",
+                            "ControllerCode": "TEST1",
+                            "PreferenceCode": "Root.Delete",
+                            "Name": "don't delete me",
+                            "Description": "don't delete me... (*)",
+                            "Status": "Hidden"
+                        },
+                        {
+                            "__type": "eventdata.co.uk.PreferenceJSON",
+                            "ControllerCode": "TEST1",
+                            "PreferenceCode": "Root.Events",
+                            "Name": "Events",
+                            "Description": "Do you want to read more about Events (*)?",
+                            "Status": "Active"
+                        },
+                        {
+                            "__type": "eventdata.co.uk.PreferenceJSON",
+                            "ControllerCode": "TEST1",
+                            "PreferenceCode": "Root.Exhibitions",
+                            "Name": "Exhibitions",
+                            "Description": "Would you like to hear periodic updates on our exhibitions (*)?",
+                            "Status": "Active"
+                        },
+                        {
+                            "__type": "eventdata.co.uk.PreferenceJSON",
+                            "ControllerCode": "TEST1",
+                            "PreferenceCode": "Root.Exhibitions.HealthCare",
+                            "Name": "Heath Care",
+                            "Description": "Would you like to hear periodic updates about our Health Care exhibition  (*)?",
+                            "Status": "Active"
+                        },
+                        {
+                            "__type": "eventdata.co.uk.PreferenceJSON",
+                            "ControllerCode": "TEST1",
+                            "PreferenceCode": "Root.Exhibitions.HealthCare.3rdPartyPartnerCommLtd",
+                            "Name": "3rd Party - Partner Comm Ltd.",
+                            "Description": "Is it OK to share information with Partner Comm Ltd. to hear about their service  (*)?",
+                            "Status": "Active"
+                        },
+                        {
+                            "__type": "eventdata.co.uk.PreferenceJSON",
+                            "ControllerCode": "TEST1",
+                            "PreferenceCode": "Root.Exhibitions.Training",
+                            "Name": "Training",
+                            "Description": "Would you like to hear about training?",
+                            "Status": "Active"
+                        }
+                    ],
+                    "Result": "success",
+                    "Message": "Loaded"
+                }
             }
-        ],
-        "Result": "success",
-        "Message": "Loaded"
+        }
     }
-}
-}
 </script>
--- JET-Test/main.js
+++ JET-Test/main.js
@@ -1,3 +1,7 @@
-VUe.component('App', function() {
-    
+import Vue from Vue;
+import App from 'App.vue'
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
 });
